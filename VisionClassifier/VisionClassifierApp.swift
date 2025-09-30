71513b4952239ec048ecd8cf5941c7b4e121ad47
pyork2022 <pyork2022@fau.edu>
2025-09-29 20:11:11 -0400

    First commit

---
+++ VisionClassifier/VisionClassifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionClassifierApp.swift
+//  VisionClassifier
+//
+//  Created by Phillip York on 9/29/25.
+//
+
+import SwiftUI
+
+@main
+struct VisionClassifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
